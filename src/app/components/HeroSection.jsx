6c1d94e424d181387b1561062e912bd3769dad2f
Arya Shakya <ryaashakya@gmail.com>
2024-02-13 21:53:59 +0545

    Add typing animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
@@ -7,20 +9,36 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-8 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 sm:text-5xl text-6xl font-extrabold">
-            Hi, I&apos;m Arya
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hi, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Arya Shakya",
+                1000,
+                "Web Developer",
+                1000,
+                "Flutter Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg lg:text-xl mb-6">
-            Hi 👋, I&apos;m Arya Shakya, final year student currently pursuing
-            Computer Engineering degree at Kathmandu University, Dhulikhel.
+            A final year student currently pursuing Computer Engineering degree
+            at Kathmandu University, Dhulikhel.
             <br />
             <br />I have always been curious and fascinated by technology.
             During the learning process, I have learned about different
-            programming languages and technologies like Python, Flutter, C++, JavaScript,
-            etc. by working on related projects. Currently, I am exploring
-            Machine Learning and Deep Learning as well.
+            programming languages and technologies like Python, Flutter, C++,
+            JavaScript, etc. by working on related projects. Currently, I am
+            exploring Machine Learning and Deep Learning as well.
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
               Contact Me
             </button>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
