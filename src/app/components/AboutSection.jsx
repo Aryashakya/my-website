42edadc2ba5a98b7b4ffa9714e13ffd1b43e653f
Arya Shakya <ryaashakya@gmail.com>
2024-02-27 17:30:02 +0545

    Complete About section

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,14 +1,94 @@
-import React from "react";
+"use client";
+import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python</li>
+        <li>Flutter</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Javascript</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul>
+        <li>BE in Computer Engineering</li>
+        <li>Kathmandu University, Dhulikhel</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Python</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        {/* <Image></Image> */}
-        <div>
-          <h2>About Me</h2>
-          <p></p>
+        <Image
+          src="/images/heroimage.png"
+          width={500}
+          height={500}
+          alt=""
+        ></Image>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg ">
+            I have always been curious and fascinated by technology. During the
+            learning process, I have learned about different programming
+            languages and technologies like C++, JavaScript, Flutter,etc. by
+            working on related projects. Currently, I am exploring Machine
+            Learning and Deep Learning as well.
+          </p>
+          <div className="flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              Skills
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              Education
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              Certifications
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
         </div>
       </div>
     </section>
